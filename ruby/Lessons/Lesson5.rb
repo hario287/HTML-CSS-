fc9efe0ba645ce6bdf3d9ff4b3129a6193228c53
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-08 16:24:30 +0000

    [Start] 2-6

--- ruby/Lessons/Lesson5.rb
+++ ruby/Lessons/Lesson5.rb
@@ -1,4 +1,4 @@
-total_price = "100"
+total_price = 100
 
 if total_price > 100
   puts "みかんを購入。所持金に余りあり。"
@@ -6,6 +6,6 @@ if total_price > 100
 elsif total_price == 100
   puts "みかんを購入。所持金は0。"
 
-else total_price < 100
+else # total_price < 100 とは書かないでいい
   puts "みかんを購入することができません。"
 end
